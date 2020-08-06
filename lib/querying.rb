86143d09b930b3f2cda13aa2f8c2e809a58e73de
Jack Quinoa <jquinoa208@gmail.com>
2020-08-06 23:17:02 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,11 +12,11 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors JOIN subgenres WHERE sub"
+  "SELECT authors.name, subgenres.name FROM authors JOIN subgenres WHERE "
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series ORDER BY "
 end
 
 def select_character_names_and_number_of_books_they_are_in
